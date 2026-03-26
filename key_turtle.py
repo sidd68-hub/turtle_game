a89516c1b0e0c2882f066c5864af0d001bc4e009
Siddharth Nahar <77606988+sidd68-hub@users.noreply.github.com>
2026-03-27 00:15:33 +0530

    Programmed Key Listen through turtle

--- key_turtle.py
+++ key_turtle.py
@@ -6,8 +6,28 @@ screen = Screen()
 def move_forwards():
     tim.forward(10)
 
-screen.listen()
+def move_backward():
+    tim.backward(10)
 
+def counter_clockwise():
+    tim.left(90)
+
+
+def clock_wise():
+    tim.right(90)
+
+def clear():
+    tim.clear() 
+    tim.penup()
+    tim.home() 
+    tim.pendown()
+
+screen.listen()
+screen.onkey(key='W', fun=move_forwards)
+screen.onkey(key='S',fun=move_backward)
+screen.onkey(key='A',fun=counter_clockwise)
+screen.onkey(key='D',fun=clock_wise)
+screen.onkey(key='C',fun=clear)
 
 
 screen.exitonclick()
